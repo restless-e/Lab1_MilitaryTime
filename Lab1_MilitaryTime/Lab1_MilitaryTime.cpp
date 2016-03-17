8817e68970db4643eb9d223636cfead7d37e3699
restless <restless.e@gmail.com>
2016-03-17 03:23:22 -0700

    First upload of git

---
+++ Lab1_MilitaryTime/Lab1_MilitaryTime.cpp
@@ -0,0 +1,15 @@
+// Lab1_MilitaryTime.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Things" << endl;
+
+    return 0;
+}
+
